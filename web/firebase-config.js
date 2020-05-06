1096cacf01fe9ec31a7288892c836e0419c15dc2
Josh Adams <joshua.william.adams@gmail.com>
2020-05-06 09:57:30 +0800

    connect project to firebase

---
+++ web/firebase-config.js
@@ -0,0 +1,10 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyA9GILVPMmUZYhJVjB3VIiBMGpa77Kji8M",
+  authDomain: "test-flutter-project-87e9d.firebaseapp.com",
+  databaseURL: "https://test-flutter-project-87e9d.firebaseio.com",
+  projectId: "test-flutter-project-87e9d",
+  storageBucket: "test-flutter-project-87e9d.appspot.com",
+  messagingSenderId: "1014571309570",
+  appId: "1:1014571309570:web:94f002c0dcd0d20d35e51b",
+  measurementId: "G-G6BEY06RJ3"
+};
